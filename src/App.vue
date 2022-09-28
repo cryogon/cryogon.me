ff78b51f6b8011a1b4a3a4fab1fb984a95b6c923
JatinThakur <jtkalga@gmail.com>
2022-09-28 23:34:11 +0530

    Added Few New Files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
+console.log(import.meta.env.BASE_URL);
 </script>
 
 <template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: "/projects",
       name: "projects",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/ProjectsView.vue"),
+    },
+    {
+      path: "/osu",
+      name: "osu",
+      component: () => import("@/views/OsuView.vue"),
     },
   ],
 });
